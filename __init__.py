09d938029166811729317ba9f40fa1494237fa01
ahmed121312 <112648546+ahmed121312@users.noreply.github.com>
2024-05-21 12:23:55 +0300

    __init__.py

--- __init__.py
+++ __init__.py
@@ -5,3 +5,6 @@ class DataPreprocessor:
 
     def standardize_data(self ,df,columns, method='minmax'):
         return Scaler.standardize_data(df,columns, method)
+
+    def handle_missing_values(self, df,columns, strategy='mean',value:any= None):
+        return MissingValueHandler.handle_missing_values(df,columns, strategy,value)
