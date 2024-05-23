c8f75df625171bcf15b1a77c9f089d33dec42f0c
ahmed121312 <112648546+ahmed121312@users.noreply.github.com>
2024-05-23 10:06:58 +0300

    data_preprocessor.py

--- pythub-main/data_preprocessing_lib/data_preprocessor.py
+++ pythub-main/data_preprocessing_lib/data_preprocessor.py
@@ -1,13 +1,1 @@
-from MissingValueHandler import MissingValueHandler
-from Scaler import Scaler
 
-
-class DataPreprocessor:
-
-    @staticmethod
-    def standardize_data(self, df, columns, method='minmax'):
-        return Scaler.standardize_data(df, columns, method)
-
-    @staticmethod
-    def handle_missing_values(self, df, columns, strategy='mean', value: any = None):
-        return MissingValueHandler.handle_missing_values(df, columns, strategy, value)
