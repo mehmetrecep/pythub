10dff386dc314533b11782c34796fad6e2cc280e
mehmetrecep <73506878+mehmetrecep@users.noreply.github.com>
2024-05-23 13:05:53 +0300

    Delete data_preprocessing_lib directory

--- data_preprocessing_lib/feature_engineer.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-
-class FeatureEngineer:
-    @staticmethod
-    def create_feature(df, column, func):
-        df[column + '_feature'] = df[column].apply(func)
-        return df
