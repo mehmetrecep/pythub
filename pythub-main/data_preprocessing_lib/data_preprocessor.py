c3891e5a92e2d1fb0032952c399ed16dd811a6bf
ahmed121312 <112648546+ahmed121312@users.noreply.github.com>
2024-05-23 16:43:41 +0300

    data_preprocessor.py

--- pythub-main/data_preprocessing_lib/data_preprocessor.py
+++ pythub-main/data_preprocessing_lib/data_preprocessor.py
@@ -1,1 +1,18 @@
+class Changing_value:
+   @staticmethod
+    def change_value(df: pd.DataFrame, columns: List[str], old_value: Any, new_value:Any) -> pd.DataFrame:
+       """
+        Replace all possible missing values in the DataFrame with a specified value.
 
+        Parameters:
+        df (pd.DataFrame): The DataFrame to process.
+        columns (List[str]): List of column names to change.
+        old_value (Any): The value will be change.
+        new_value (Any):The value to which we will change.
+
+       Returns:
+        pd.DataFrame: The DataFrame with the new values.
+        """
+        for col in columns:
+            df[col].replace(old_value,new_value،inplace=True)
+        return df
