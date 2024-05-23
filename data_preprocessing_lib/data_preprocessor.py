705efc63dbff7c7c242c249c8be4504d46dcd21f
ahmed121312 <112648546+ahmed121312@users.noreply.github.com>
2024-05-23 16:32:56 +0300

    changing_value.py

--- data_preprocessing_lib/data_preprocessor.py
+++ data_preprocessing_lib/data_preprocessor.py
@@ -1,1 +1,7 @@
-
+class Changing_value:
+   @staticmethod
+    def change_value(df: pd.DataFrame, columns: List[str], old_value: Any, new_value:Any) -> pd.DataFrame:
+       
+        for col in columns:
+            df[col].replace(old_value,new_value،inplace=True)
+        return df
